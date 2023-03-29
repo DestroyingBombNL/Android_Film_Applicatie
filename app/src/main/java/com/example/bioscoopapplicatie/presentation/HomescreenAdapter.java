0bde2315f30019cd800a95a811b9ee1f9523f9f6
Kenanvdh <kenanvdh@ziggo.nl>
2023-03-29 16:29:02 +0200

    User klasse

--- app/src/main/java/com/example/bioscoopapplicatie/presentation/HomescreenAdapter.java
+++ app/src/main/java/com/example/bioscoopapplicatie/presentation/HomescreenAdapter.java
@@ -13,6 +13,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.bioscoopapplicatie.R;
+import com.example.bioscoopapplicatie.domain.Media;
 import com.example.shareameal.R;
 import com.example.shareameal.domain.Meal;
 
@@ -21,7 +23,7 @@ import java.util.List;
 
 public class HomescreenAdapter extends RecyclerView.Adapter<HomescreenAdapter.MediaViewHolder> {
     private static final String TAG = HomescreenAdapter.class.getSimpleName();
-    private List<Meal> meals;
+    private List<Media> media;
     private LayoutInflater inflater;
     private Context context;
 
@@ -35,30 +37,26 @@ public class HomescreenAdapter extends RecyclerView.Adapter<HomescreenAdapter.Me
     public MediaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Log.d(TAG, "onCreateViewHolder");
         // Inflate an item view.
-        View mItemView = inflater.inflate(R.layout.meal_item_menu, parent, false);
+        View mItemView = inflater.inflate(R.layout., parent, false);
         return new MediaViewHolder(mItemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MediaViewHolder holder, int position) {
         Log.d(TAG, "onBindViewHolder nr " + position);
-        Meal meal = meals.get(position);
-        Glide.with(holder.itemView).load(meal.getImageUrl()).into(holder.image);
-        holder.name.setText(meal.getName());
-        holder.cityDate.setText("Zwijndrecht / " + meal.getDate());
-        holder.price.setText("€" + String.valueOf(meal.getPrice()));
+
     }
 
     @Override
     public int getItemCount() {
-        if(this.meals == null)
+        if(this.media == null)
             return 0;
-        else return meals.size();
+        else return media.size();
     }
 
-    public void setData(ArrayList<Meal> meals) {
+    public void setData(ArrayList<Media> media) {
         Log.d(TAG, "setData(meals)");
-        this.meals = meals;
+        this.media = media;
         notifyDataSetChanged();
     }
 
@@ -72,32 +70,14 @@ public class HomescreenAdapter extends RecyclerView.Adapter<HomescreenAdapter.Me
 
         public MediaViewHolder(@NonNull View itemView) {
             super(itemView);
-
-            image = itemView.findViewById(R.id.meal_item_menu_img);
-            name = itemView.findViewById(R.id.meal_item_menu_name_txt);
-            cityDate = itemView.findViewById(R.id.meal_item_menu_city_date_txt);
-            price = itemView.findViewById(R.id.meal_item_menu_price_txt);
-
             itemView.setOnClickListener(this);
         }
-
 
         @Override
         public void onClick(View view) {
             Log.d(TAG, "ViewHolder onClick - listitem nr " + getAdapterPosition());
-            Meal currentMeal = meals.get(getAdapterPosition());
-            Intent detailsIntent = new Intent(context, DetailsMedia.class);
-            detailsIntent.putExtra("id", currentMeal.getId());
-            detailsIntent.putExtra("name", currentMeal.getName());
-            detailsIntent.putExtra("description", currentMeal.getDescription());
-            detailsIntent.putExtra("isActive", currentMeal.getActive());
-            detailsIntent.putExtra("isVega", currentMeal.getVega());
-            detailsIntent.putExtra("isVegan", currentMeal.getVegan());
-            detailsIntent.putExtra("isToTakeHome", currentMeal.getToTakeHome());
-            detailsIntent.putExtra("dateTime", currentMeal.getDateTime());
-            detailsIntent.putExtra("maxAmountOfParticipants", currentMeal.getMaxAmountOfParticipants());
-            detailsIntent.putExtra("price", currentMeal.getPrice());
-            detailsIntent.putExtra("imageUrl", currentMeal.getImageUrl());
+            Media currentMedia = media.get(getAdapterPosition());
+
             detailsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(detailsIntent);
         }
