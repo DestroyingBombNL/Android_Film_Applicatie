cd21c414c271652c940a92bb98db571b7a013e05
Sven <Svenhu@live.nl>
2023-03-29 11:57:28 +0200

    Up to date version

--- app/src/main/java/com/example/bioscoopapplicatie/MainActivity.java
+++ app/src/main/java/com/example/bioscoopapplicatie/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.bioscoopapplicatie;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
